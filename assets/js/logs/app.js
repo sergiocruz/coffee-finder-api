074f3301ab8fa9a7de41d797aae2ba39794c9a4c
Sergio Cruz <sergio@cruzcorp.net>
2014-06-18 14:59:43 -0400

    Fixed gitignore issue

---
+++ assets/js/logs/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// /**
+// * logsApp Module
+// *
+// * Description
+// */
+var app = angular.module('pebbleLogsApp', ['ngResource']);
+
+app.controller('MainCtrl', ['$scope', 'LogsResource', function MainCtrl($scope, LogsResource) {
+
+  $scope.logs = [];
+
+  function logsResourceLoad(logs) {
+    console.log(logs);
+    $scope.logs = logs;
+  }
+
+  function logsResourceError() {
+    console.log('error', arguments);
+  }
+
+  LogsResource
+    .query()
+      .$promise
+        .then(logsResourceLoad)
+        .catch(logsResourceError);
+
+  
+}]);
+
+app.provider('LogsResource', function LogsResource() {
+    this.$get = ['$resource', function($resource) {
+      var LogsResource = $resource(
+        '/logs/api',
+        {}
+      );
+
+      return LogsResource;
+    }];
+  }
+);
+
+
+
+// Currency with no decimal points
+app.filter('toPrecision', function filterToPrecision() {
+  return function(number, precision) {
+
+    // Default precision is 6
+    precision = precision ? parseInt(precision, 10) : 6;
+
+    // Returns converted
+    return parseFloat(number).toPrecision(precision);
+  };
+});
